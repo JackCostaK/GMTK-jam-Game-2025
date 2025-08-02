0d09352a7d83773f414eb9ff354abaeb93e84497
Jack <jackck1313@gmail.com>
2025-08-01 23:47:14 -0400

    nothing

--- LoopGame/Assets/Scripts/PlayerMovement.cs
+++ LoopGame/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerCtrl : MonoBehaviour
     float speedX, speedY;
     Rigidbody2D rb;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,23 +18,28 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
         rb.velocity = new Vector2(speedX, speedY);
 
-        if(speedY != 0){
+        if (speedY != 0)
+        {
             animator.SetBool("IsWalkingUp", true);
             print("Up");
         }
-        else if(speedX < 0){
+        else if (speedX < 0)
+        {
             animator.SetBool("IsWalkingLeft", true);
 
         }
-        else if(speedX > 0){
+        else if (speedX > 0)
+        {
             animator.SetBool("IsWalkingRight", true);
             print("Right");
         }
-        else{
+        else
+        {
             animator.SetBool("IsWalkingLeft", false);
             animator.SetBool("IsWalkingRight", false);
             animator.SetBool("IsWalkingUp", false);
@@ -41,3 +47,4 @@ public class PlayerCtrl : MonoBehaviour
 
     }
 }
+
