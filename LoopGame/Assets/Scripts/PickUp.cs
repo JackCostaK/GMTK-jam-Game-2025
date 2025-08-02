1bd3667ed62dac7259fe0618ff8665ce74aa5af9
tylerrwen <75301296+Sh0w3r15@users.noreply.github.com>
2025-08-02 16:27:06 -0400

    popas[dos

---
+++ LoopGame/Assets/Scripts/PickUp.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform HoldSpot;
+    public LayerMask PickUpMask;
+    public Vector3 Direction {get; set;}
+    public GameObject DestroyEffect;
+    private GameObject itemHolding;
+    public FollowThePath pathfollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(itemHolding)
+            {
+                itemHolding.transform.position = transform.position + Direction;
+                itemHolding.transform.parent = null;
+                if(itemHolding. GetComponent<Rigidbody2D>())
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+                itemHolding = null;
+            }
+            else
+            {
+                Collider2D PickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, PickUpMask);
+                if(PickUpItem)
+                {
+                    itemHolding = PickUpItem.gameObject;
+                    itemHolding.transform.position = HoldSpot.position;
+                    itemHolding.transform.parent = transform;
+                    if (itemHolding.GetComponent<Rigidbody2D>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if(itemHolding)
+            {
+                StartCoroutine(ThrowItem(itemHolding));
+                itemHolding = null;
+            }
+        }
+    }
+
+    IEnumerator ThrowItem(GameObject item)
+    {
+        Vector3 StartPoint = item.transform.position;
+        Vector3 EndPoint = transform.position + Direction * 8;
+        item.transform.parent = null;
+        for (int i =0; i < 25; i++)
+        {
+            item.transform.position = Vector3.Lerp(StartPoint, EndPoint, i * .04f);
+            yield return null;
+        }
+        if (item.GetComponent<Rigidbody2D>())
+            item.GetComponent<Rigidbody2D>().simulated = true;
+        Instantiate(DestroyEffect, item.transform.position, Quaternion.identity);
+        Destroy(DestroyEffect);
+        Destroy(item);
+    }
+}
--- LoopGame/Assets/Scripts/PlayerMovement.cs
+++ LoopGame/Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,27 @@ public class PlayerCtrl : MonoBehaviour
     public float speed;
     float speedX, speedY;
     Rigidbody2D rb;
+    private Vector3 Change;
+    private PickUp PickUp;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        PickUp = gameObject.GetComponent<PickUp>();
+        PickUp.Direction = new Vector2(0, -1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Change = Vector3.zero;
+        Change.x = Input.GetAxisRaw("Horizontal");
+        Change.y = Input.GetAxisRaw("Vertical");
+        if(Change.sqrMagnitude > .1f)
+        {
+            PickUp.Direction = Change.normalized;       
+        }
 
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
@@ -31,6 +42,7 @@ public class PlayerCtrl : MonoBehaviour
         else if (speedX < 0)
         {
             animator.SetBool("IsWalkingLeft", true);
+            print("Left");
 
         }
         else if (speedX > 0)
