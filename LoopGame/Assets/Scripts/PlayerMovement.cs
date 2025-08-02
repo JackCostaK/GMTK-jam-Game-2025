8232254b940c2c999379385226edf4c2677d90e6
tylerrwen <75301296+Sh0w3r15@users.noreply.github.com>
2025-08-01 22:51:51 -0400

    Fuck

--- LoopGame/Assets/Scripts/PlayerMovement.cs
+++ LoopGame/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public Animator animator;
     public float speed;
     float speedX, speedY;
     Rigidbody2D rb;
@@ -19,5 +20,24 @@ public class PlayerCtrl : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
         rb.velocity = new Vector2(speedX, speedY);
+
+        if(speedY != 0){
+            animator.SetBool("IsWalkingUp", true);
+            print("Up");
+        }
+        else if(speedX < 0){
+            animator.SetBool("IsWalkingLeft", true);
+
+        }
+        else if(speedX > 0){
+            animator.SetBool("IsWalkingRight", true);
+            print("Right");
+        }
+        else{
+            animator.SetBool("IsWalkingLeft", false);
+            animator.SetBool("IsWalkingRight", false);
+            animator.SetBool("IsWalkingUp", false);
+        }
+
     }
 }
